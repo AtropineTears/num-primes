111e13271ee624477c30f7c6d594a3731d2f3b93
Atropine <51803887+0xSilene@users.noreply.github.com>
2021-08-14 16:20:57 -0400

    Fixed Part of Issue 1

---
+++ tests/bug1.rs
@@ -0,0 +1,37 @@
+use num_primes::*;
+use num_bigint::BigUint;
+use num_traits::{Zero, One};
+
+
+// ===Issue #1===
+// - Found on 12 Aug 2021
+// The bug is that verification of small prime numbers does not work correctly. It also hangs when generating a 14 bit prime number.
+// This affects both Verfication::is_prime() and Verification::is_composite() as is_composite simply is the inverse of the primality check
+
+#[test]
+fn bug1() {
+    let numbers = [
+        Generator::new_prime(512),
+        Generator::new_prime(32),
+        Generator::new_prime(16),
+        // Two prime numbers
+        17957u32.into(),
+        5u32.into(),
+    ];
+
+    for number in numbers {
+        if Verification::is_prime(&number) {
+            println!("{} is a prime number", number);
+        }
+        else {
+            println!("{} is not a prime number", number);
+        }
+    }
+}
+
+#[test]
+fn bug1_2(){
+    // Bug 1 
+    let hang = Generator::new_prime(14);
+    println!("{} is not a prime number", hang);
+}
