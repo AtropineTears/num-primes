92ae30f0858ec0c73188ebd64fa3ab573fd9bb0e
Evan Hines <evrhines@gmail.com>
2021-12-06 23:27:00 -0500

    Fix bug in small prime factor code. For some reason it would check modulus the prime and return maybe prime although we can just as easily check if it actually that prime right here. This was causing small primes to go through the more expensive primality tests although we didn't need to.
    Add a test for factoring two prime numbers

--- tests/factorization.rs
+++ tests/factorization.rs
@@ -2,18 +2,17 @@ use num_primes::{Generator,Factorization};
 
 #[test]
 fn factor_uint(){
-    // Generate a Large Unsigned Integer of 32 bits
-    let x = Generator::new_uint(32);
-
-    println!("Number: {}",x);
+    // Generate a Unsigned Integer of 16 bits
+    let p = Generator::new_prime(16);
+    let q = Generator::new_prime(16);
+    let f = &p * &q;
 
     // Factor The Largest Prime of x
-    let prime_factor = Factorization::prime_factor(x);
+    let prime_factor = Factorization::prime_factor(f).unwrap();
 
-    // Print Out The Statements
-    match prime_factor {
-        Some(prime_factor) => println!("Prime Factor: {}",prime_factor),
-        None => println!("There are no prime factors")
+    if &p > &q {
+        assert_eq!(prime_factor, p);
+    } else {
+        assert_eq!(prime_factor, q);
     }
-
 }
